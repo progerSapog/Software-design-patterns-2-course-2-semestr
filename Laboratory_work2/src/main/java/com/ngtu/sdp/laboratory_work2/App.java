2754b9c1f746a6d51c56f18d117116d8aa99cadc
Vladislav Sapozhnikov <vlad.sapozhnickof@gmail.com>
2021-04-24 16:43:00 +0300

    Maven project test

---
+++ Laboratory_work2/src/main/java/com/ngtu/sdp/laboratory_work2/App.java
@@ -0,0 +1,13 @@
+package com.ngtu.sdp.laboratory_work2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "ОНО РАБОТАЕ!!!! УРА" );
+    }
+}
